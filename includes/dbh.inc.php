5f7a8bdfe1a5d0fcf508fb447c0f544ab2b6b559
barvyleta <barvyleta@users.noreply.github.com>
2017-12-12 13:37:54 -0800

    Removed database info
    Don't need this public!

--- includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServerName = "localhost";
-$dbUserName = "root";
-$dbPassword = "root";
-$dbName = "test";
-
-$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName, 1339);
